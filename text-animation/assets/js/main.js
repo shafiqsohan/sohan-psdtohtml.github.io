d7f88149f6344dc5d869f6375afd0b27b562b125
shafiqsohan <shafiqsohan@users.noreply.github.com>
2017-07-18 00:35:55 +0600

    sssss

---
+++ text-animation/assets/js/main.js
@@ -0,0 +1,32 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+        
+//        animation Text One
+//        ------------------
+        setTimeout(function() {
+            $(".text-animate-1 ul").removeClass("hiddenz"); 
+        }, 500);
+        
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
